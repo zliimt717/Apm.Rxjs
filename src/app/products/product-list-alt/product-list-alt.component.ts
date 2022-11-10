93d7bf175b6c8ea3eb57e7273c5612b887cc5237
xin.gu <xin.gu@accenture.com>
2022-11-10 18:04:41 +0100

    add some codes

--- src/app/products/product-list-alt/product-list-alt.component.ts
+++ src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { catchError, EMPTY, Subscription } from 'rxjs';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
@@ -9,22 +9,22 @@ import { ProductService } from '../product.service';
   selector: 'pm-product-list',
   templateUrl: './product-list-alt.component.html'
 })
-export class ProductListAltComponent implements OnInit, OnDestroy {
+export class ProductListAltComponent implements OnDestroy {
   pageTitle = 'Products';
   errorMessage = '';
   selectedProductId = 0;
+
+  products$=this.productService.products$ .pipe(
+    catchError(err=>{
+      this.errorMessage=err;
+      return EMPTY;
+    })
+   );
 
   products: Product[] = [];
   sub!: Subscription;
 
   constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.sub = this.productService.getProducts().subscribe({
-      next: products => this.products = products,
-      error: err => this.errorMessage = err
-    });
-  }
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
